5e05086452dd3a0cba754fa06fc6f8ada7c2a5a4
Arunsh93 <Arunhosur93@gmail.com>
2021-09-27 12:21:59 +0530

    Employee Wages Computations Using OOPS

---
+++ EmployeeWagesUsingOOPS/MainMethodClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWagesUsingOOPS
+{
+    class MainMethodClass
+    {
+         static void Main(string[] args)
+         {
+            //UC1_PresentOrAbsent obj1 = new UC1_PresentOrAbsent();
+            //obj1.CheckEmp();
+
+            //UC2_CalculateWages obj2 = new UC2_CalculateWages();
+            //obj2.DailyWages();
+
+            //UC3_AddPartTime obj3 = new UC3_AddPartTime();
+            //obj3.AddPartTimeEmp();
+
+            //UC4_EmpWageUsingSwitchCase obj4 = new UC4_EmpWageUsingSwitchCase();
+            //obj4.EmpWageUsingSwitchCase();
+
+            UC5_EmpWagesWorkingInMonth obj5 = new UC5_EmpWagesWorkingInMonth();
+            obj5.EmpWagesWorkingInMonth();
+         }
+    }
+}
